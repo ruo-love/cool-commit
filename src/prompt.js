bc4eaa03827f8204e11de266f172aae6d7bdf938
qiancheng.zhao <qiancheng.zhao@alo7.com>
2025-11-27 14:38:20 +0800

    💻feat: update version and fix commit message format <🐥 2025-11-27 02:38:17>

--- src/prompt.js
+++ src/prompt.js
@@ -50,12 +50,12 @@ function getPrompt(options){
     你是一个资深程序员，请根据下面的 git diff 自动生成高质量 commit message。
     要求：
     1. 使用 Conventional Commit 格式以"${prefix_icon}: " 开头
-    2. commit 以" <⏰ ${updateTime}> "结尾,时间格式 YYYY-MM-DD mm:hh:ss
+    2. commit 以" <🐥 ${updateTime}> "结尾,时间格式 YYYY-MM-DD mm:hh:ss
     3. 保持简洁、语义清晰,不要出现换行
     4. 不要解释，不要生成多余文本
     5. 示例：
-      💻feat: 移除多余的功能 <⏰ 2025-11-26 07:14:43>
-      🔧fix: 修复录音问题 <⏰ 2025-11-26 07:14:43>
+      💻feat: 移除多余的功能 <🐥 2025-11-26 07:14:43>
+      🔧fix: 修复录音问题 <🐥 2025-11-26 07:14:43>
     6. 请将 commit message 用简洁、专业的${lang}语音生成"
     === DIFF START ===
     ${diff}
@@ -68,7 +68,7 @@ export function getStyleMessage(options){
   const date = new Date()
   const updateTime = date.toLocaleString()
   const prefix_icon = getIcon(prefix)+prefix 
-  return `${prefix_icon}: ${message} <⏰ ${updateTime}>`
+  return `${prefix_icon}: ${message} <🐥 ${updateTime}>`
 }
 
 export default getPrompt;
